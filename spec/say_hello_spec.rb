5ee4742aecc03770d7512ca36fbb4029ce52633a
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-06-20 18:04:15 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,4 +1,4 @@
-describe "say_hello" do 
+describe "say_hello" do
 
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
